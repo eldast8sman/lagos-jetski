158f13b972619f655448b71fe7f25764cb089da5
Omotolani Olurotimi <omotolaniolurotimi@gmail.com>
2024-07-16 15:25:56 +0100

    Include Python Installation

--- .platform/hooks/prebuild/install_superisor.sh
+++ .platform/hooks/prebuild/install_superisor.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-sudo python -m venv /opt/supervisor
+sudo apt-get update
+sudo apt-get install python3 python3-venv python3-pip -y
+
+sudo python3 -m venv /opt/supervisor
+. /opt/supervisor/bin/activate
 sudo /opt/supervisor/bin/pip install --upgrade pip
 sudo /opt/supervisor/bin/pip install supervisor certbot
 sudo ln -sf /opt/supervisor/bin/supervisord /usr/bin/supervisord
