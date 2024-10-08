7fb50324fbfb7d26a0cff7ae7f3d55627f7d9afc
Omotolani Olurotimi <omotolaniolurotimi@gmail.com>
2024-10-09 00:29:22 +0100

    Sparkle

--- routes/console.php
+++ routes/console.php
@@ -9,12 +9,12 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::call(function(){
-    $repo = new MemberRepository(new User());
-    $repo->fetch_g5_customers();
-})->twiceDaily();
+// Schedule::call(function(){
+//     $repo = new MemberRepository(new User());
+//     $repo->fetch_g5_customers();
+// })->twiceDaily();
 
-Schedule::call(function(){
-    $repo = new MenuRepository(new Product());
-    $repo->fetch_g5_menu();
-})->twiceDaily();
+// Schedule::call(function(){
+//     $repo = new MenuRepository(new Product());
+//     $repo->fetch_g5_menu();
+// })->twiceDaily();
