690da4fa2845e59d8c191faa4cffe57428ccd2a5
Omotolani Olurotimi <omotolaniolurotimi@gmail.com>
2024-10-18 10:52:10 +0100

    Wallet

--- app/Repositories/Interfaces/WalletRepositoryInterface.php
+++ app/Repositories/Interfaces/WalletRepositoryInterface.php
@@ -2,9 +2,13 @@
 
 namespace App\Repositories\Interfaces;
 
+use App\Models\User;
+
 interface WalletRepositoryInterface extends AbstractRepositoryInterface
 {
     public function userTransactions(int $id, int $limit=10);
 
     public function userTransaction(string $uuid, int $user_id);
+
+    public function fetch_wallet(User $user);
 }
