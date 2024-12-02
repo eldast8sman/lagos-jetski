2d58611d81a4ae06c0ee9c944fdc96612953297e
Omotolani Olurotimi <omotolaniolurotimi@gmail.com>
2024-12-02 11:14:22 +0100

    Laravel

--- .platform/hooks/postdeploy/01_run_artisan.sh
+++ .platform/hooks/postdeploy/01_run_artisan.sh
@@ -23,7 +23,7 @@ sudo chown -R webapp:webapp storage bootstrap/cache
 sudo chmod -R 775 storage bootstrap/cache
 
 # Run Laravel Artisan commands
-php artisan migrate
+php artisan migrate --force
 php artisan config:clear
 php artisan cache:clear
 php artisan view:clear
